ea5ee30f0b5ea4a431ec9429200e4ae221037276
Fickry Iman <fickry.iman@gmail.com>
2023-09-25 21:25:02 +0700

    shorten the routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,11 @@ Rails.application.routes.draw do
   # root "articles#index"
   root "users#index"
 
-  get "/users", to: "users#index", as: "users"
-  get "/users/:id", to: "users#show", as: "user"
-  get "/users/:user_id/posts", to:"posts#index", as: "user_posts"
-  get "/users/:user_id/posts/:id", to: "posts#show", as: "user_post"
+  # get "/users", to: "users#index", as: "users"
+  # get "/users/:id", to: "users#show", as: "user"
+  # get "/users/:user_id/posts", to:"posts#index", as: "user_posts"
+  # get "/users/:user_id/posts/:id", to: "posts#show", as: "user_post"
+  resources :users, only:[:index, :show] do
+    resources :posts, only: [:index, :show]
+  end
 end
