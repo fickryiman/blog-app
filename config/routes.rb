ee8f863e1654d0cc0ef662bf1c03f59cc4dc801e
Fickry Iman <fickry.iman@gmail.com>
2023-09-25 08:42:00 +0700

    handle only 4 required url
    - get "/users", to: "users#index", as: "users"
    - get "/users/:id", to: "users#show", as: "user"
    - get "/users/:user_id/posts", to:"posts#index", as: "user_posts"
    - get "/users/:user_id/posts/:id", to: "posts#show", as: "user_post"
    end

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,6 @@ Rails.application.routes.draw do
 
   get "/users", to: "users#index", as: "users"
   get "/users/:id", to: "users#show", as: "user"
-  # get "/users/:user_id/posts", to:"posts#show", as: "user_posts"
-  # get "/users/:user_id/posts/:id", to: "posts#index", as: "user_post"
-
-  get "/posts", to: "posts#index", as: "posts"
-  get "/posts/:id", to: "posts#show", as: "post"
+  get "/users/:user_id/posts", to:"posts#index", as: "user_posts"
+  get "/users/:user_id/posts/:id", to: "posts#show", as: "user_post"
 end
